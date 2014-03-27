c3ba3fef728f5e2328acf98269b8e04c0ebefcba
Erkkinomme <erkki.nomme@khk.ee>
2014-03-27 22:30:31 +0200

    Fix code layout. Change tag title to tags name. Fix halo bug. Fix html minor bug

--- controllers/tags.php
+++ controllers/tags.php
@@ -1,19 +1,17 @@
 <?php
-
-
 class tags extends Controller{
 
     function index(){
-        $this->tag = get_all("SELECT tag_name, COUNT(post_id) AS count
+        $this->tags = get_all("SELECT tag_name, tag_id, COUNT(post_id) AS count
 		 FROM post_tags NATURAL JOIN tag
 		 GROUP BY tag_id");
     }
-    function view(){
 
+    function view(){
         $tag_id= $this->params[0];
         $this->posts = get_all("SELECT post.*, user.username
 		 FROM post_tags NATURAL JOIN post NATURAL JOIN user WHERE tag_id='$tag_id'
 		 ");
+        $this->tag_name = get_one("SELECT tag_name FROM tag WHERE tag_id='$tag_id'");
     }
 }
-
--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -1,23 +1,33 @@
-
 <div class="span8">
-    <h1><?=$post['post_subject']?></h1>
-    <p><?=$post['post_text']?></p>
+    <h1><?= $post['post_subject'] ?></h1>
+
+    <p><?= $post['post_text'] ?></p>
+
     <div>
-            <span class="badge badge-success">Posted <?=$post["post_created"]?> by <?=$post["username"]?>
+        <span class="badge badge-success"><?= $post['post_created'] ?></span>
+
+        <div class="pull-right"><span class="label"><?= $post['username'] ?></span>
+            <span class="label">story</span> <span class="label">blog</span>
+            <span class="label">personal</span>
+        </div>
+        <? foreach ($tags as $tag): ?>
+            <a href="<?= BASE_URL ?>tags/view/<?= $tag['tag_id'] ?>"><span class="label label-info"
+                                                                           style="background-color:#5bc0de"><?= $tag['tag_name'] ?></span></a>
+        <? endforeach ?>
     </div>
-
+    <hr>
 </div>
-<!--
-    <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_name']?>" class="list-group-item"><?=$tag['tag_name']?></a>
-    -->
-<?foreach ($tags as $tag):?><a href="<?=BASE_URL?>tags/view/<?=$tag['tag_name']?>"><span class="label-info" style="background-color: #5bc0de"><?=$tag['tag_name']?></span></a><?endforeach?>
 
-<?foreach($comments as $comment):?>
+
+<? foreach ($comments as $comment): ?>
     <div class="media">
-        <a class="pull-left" href="#"><img class="media-object" src="..." alt="..."></a>
+        <a class="pull-left" href="#">
+            <img class="media-object" src="<?= ASSETS_URL ?>img/mati.png" alt="...">
+        </a>
+
         <div class="media-body">
-            <h4 class="media-heading"><?=$post["username"]?></h4><?=$comment["comment_text"]?>
+            <h4 class="media-heading"><?= $post["username"] ?></h4>
+            <?= $comment["comment_text"] ?>
         </div>
     </div>
-<?endforeach?>
-
+<? endforeach ?>
