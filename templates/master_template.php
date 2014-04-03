688b67413157cdda834ce6ee91a455bc75791ffa
Erkkinomme <erkki.nomme@khk.ee>
2014-04-03 15:13:42 +0300

    Fix brand link

--- templates/master_template.php
+++ templates/master_template.php
@@ -40,7 +40,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="posts"><?=PROJECT_NAME?></a>
+			<a class="navbar-brand" href="<?=BASE_URL?>"><?=PROJECT_NAME?></a>
 		</div>
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
